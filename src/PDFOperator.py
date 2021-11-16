ff4c3e629ded2fcd315ffb54a3c6b69e7e2b5ced
aakash1234567 <44570564+aakash1234567@users.noreply.github.com>
2021-11-16 18:30:24 +0530

    "improvements"

--- src/PDFOperator.py
+++ src/PDFOperator.py
@@ -1,23 +1,14 @@
-from PyPDF2 import PdfFileWriter, PdfFileReader
-from flask import Flask, request,send_file
+import pikepdf
+from flask import Flask, send_file
 app = Flask(__name__)
+app.config["SRC"] = "./src"
 @app.route('/',methods=['POST'])
-def hello_world():
-    out = PdfFileWriter()
-    file = PdfFileReader(request.files['file']) 
+def pdfOperator():
     f_name = request.files['file'].filename
     password = request.form.get('password')
-    if file.isEncrypted:
-        file.decrypt(password)
-        for idx in range(file.numPages):
-            page = file.getPage(idx)
-            out.addPage(page)
-        with open("./dummy_file.pdf", "wb") as f:
-            out.write(f)
-        print("File decrypted Successfully.")
-    else:
-        print("File already decrypted.")
-    return send_file("./dummy_file.pdf",as_attachment=True,mimetype="application/pdf")
+    with pikepdf.open(request.files['file'], password=password) as pdf:
+        pdf.save(app.config["SRC"] + "/file_decrypted.pdf")
+    return send_file("./file_decrypted.pdf",as_attachment=True,mimetype="application/pdf")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
